88b61600bd65f1c64119185da877a94630baf061
sandro-lucas <sandrolucasekell@gmail.com>
2024-01-23 17:16:12 -0300

    sa

---
+++ math/script.js
@@ -0,0 +1,12 @@
+let numb1 = prompt("digite o primeiro numero");
+let numb2 = prompt("digite o segundo numero");
+let numb3 = prompt("digite o terceiro numero");
+
+let soma = numb1 + numb2
+let sub = numb1 - numb3
+let div = numb2 / numb3
+let elev = numb2 ^ 3
+console.log("Soma: "+numb1+" + "+numb2+" = "+soma)
+console.log("subtração: "+numb1+" - "+numb3+" = "+sub)
+console.log("divisão: "+numb2+" - "+numb3+" = "+div)
+console.log("elevado: "+numb2+"^ 3 = "+sub)
