fccbe79a504f9e8da7e1801807e817de2a0cfd02
sandro-lucas <sandrolucasekell@gmail.com>
2024-01-24 17:34:51 -0300

    d

---
+++ script.js
@@ -0,0 +1,13 @@
+let num = prompt("digite um valor");
+
+const numb2 = 33;
+const numb3 = 11;
+ 
+if (num > 0){ console.log( "Positivo");}
+else if ( num < 0){console.log("negtivo");}
+else if ( num == 0){console.log("nulo");}
+
+const num1 =prompt("digite ou valor")
+
+if (num1 >= 0 && num1 < 10) {return "numb2"}
+else if (num1 < 0 || num1 > 10) {return "numb3"} 
