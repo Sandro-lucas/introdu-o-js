23ad310c475e7fc9565607f5cf4cf433d5f5cd6a
Sandro-lucas <sandrolucasekell@gmail.com>
2024-02-05 15:20:20 -0300

    fqawf

--- mes2/atv.2/login/part1/script.js
+++ mes2/atv.2/login/part1/script.js
@@ -2,18 +2,24 @@ function validateLogin() {
     var username = document.getElementById("username").value;
     var password = document.getElementById("password").value;
     var loginResult = document.getElementById("loginResult");
-  
+
     // Exemplo simples de validação (usuário: "lucas", senha: "123")
-    if (username === "lucas" && password === "123") {
+    if (username === "lucas" && password === "abc") {
         loginResult.textContent = "Login bem-sucedido!";
         loginResult.style.color = "#4caf50";
         // Redireciona para a segunda tela após o login bem-sucedido
-        window.location.href = "../atv.1/exemplo1/index.html";
-    } else if (username === "" || password === "") {
+        window.location.href = "../parte2/index.html";
+    } else if (username == "" || password == "") {
         loginResult.textContent = "Por favor, preencha todos os campos!";
         loginResult.style.color = "#f44336";
     } else {
         loginResult.textContent = "Seu nome de usuário ou senha está incorreto!";
         loginResult.style.color = "#f44336";
     }
-}d3
+}
+
+// Adiciona um ouvinte de evento ao formulário para chamar a função validateLogin
+document.getElementById("loginForm").addEventListener("submit", function(event) {
+    event.preventDefault();  // Impede o envio padrão do formulário
+    validateLogin();
+});
---
+++ mes2/atv.2/login/parte2/script.js
@@ -0,0 +1,51 @@
+function calcular(operacao) {
+    // Obter valores dos campos de entrada
+    const numeroA = parseFloat(document.getElementById("numeroA").value);
+    const numeroB = parseFloat(document.getElementById("numeroB").value);
+    const num1 = numeroA + numeroB;
+    let resultado;
+  
+    // Switch para determinar a operação
+    switch (operacao) {
+        case 'soma':
+            resultado = numeroA + numeroB;
+            break;
+        case 'subtracao':
+            resultado = numeroA - numeroB;
+            break;
+        case 'multiplicacao':
+            resultado = numeroA * numeroB;
+            break;
+        case 'divisao':
+            resultado = numeroA / numeroB;
+            break;
+        case 'quadrado':
+            resultado = num1 * num1;
+            break;
+        case 'cubo':
+            resultado = num1 * num1 * num1;
+            break;
+        case 'med':
+            resultado = num1 / 2;
+            break;
+        case 'comp':
+            if (numeroA < numeroB) {
+                resultado = `${numeroA} < ${numeroB} é a multiplicação da metade do valor do maior pelo valor do menor é = ${(numeroA / 2) * numeroB}`;
+            } else if (numeroA > numeroB) {
+                resultado = `${numeroA} > ${numeroB} é a multiplicação da metade do valor do maior pelo valor do menor é = ${(numeroB / 2) * numeroA}`;
+            } else {
+                resultado = `${numeroA} = ${numeroB} é o resultado da soma dividida por 2 é = ${(num1 / 2)}`;
+            }
+            break;
+        default:
+            resultado = "Operação inválida";
+    }
+  
+    // Exibir o resultado na página
+    document.getElementById("resulte").innerHTML = "Resultado: " + resultado;
+  }
+  
+  
+  
+  
+  
