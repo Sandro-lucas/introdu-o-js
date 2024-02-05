e873fc0fff87832cabc4fa3ed8dc7020e74f0ca3
Sandro-lucas <sandrolucasekell@gmail.com>
2024-02-05 18:01:41 -0300

    aws

---
+++ mes2/atv3/academia/script.js
@@ -0,0 +1,5 @@
+
+let imcMaria = parseFloat(55 / (1.6 * 1.6).toFixed(2));
+let imcPedro = parseFloat(70 / (1.8 * 1.8).toFixed(2));
+document.getElementById('p1').innerHTML = (` Maria, com 55 kg, 1,65 m e 25 anos, seu  IMC sera ${imcMaria}`);
+document.getElementById('p2').innerHTML = (` Pedro, com 70 kg, 1,80 m e 28 anos, seu  IMC sera ${imcPedro}`)
