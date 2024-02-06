cdd3737de636f211dab3c2e876fd9a84e084872a
Sandro-lucas <sandrolucasekell@gmail.com>
2024-02-06 17:12:04 -0300

    fdqa

--- mes2/atv3/academia/script.js
+++ mes2/atv3/academia/script.js
@@ -1,5 +1,39 @@
 
-let imcMaria = parseFloat(55 / (1.6 * 1.6).toFixed(2));
-let imcPedro = parseFloat(70 / (1.8 * 1.8).toFixed(2));
-document.getElementById('p1').innerHTML = (` Maria, com 55 kg, 1,65 m e 25 anos, seu  IMC sera ${imcMaria}`);
-document.getElementById('p2').innerHTML = (` Pedro, com 70 kg, 1,80 m e 28 anos, seu  IMC sera ${imcPedro}`)
+alert ("No lugar da vírgula, coloque ponto")
+function calcular() {
+    // Obter valores dos campos de entrada
+    const peso = parseFloat(document.getElementById("peso").value);
+    const altura = parseFloat(document.getElementById("altura").value);
+    const imc = peso / (altura * altura);
+    let resultado;
+
+    // Switch para determinar a classificação do IMC
+    switch(true) {
+        case imc < 18.5 :
+            resultado = "Você está abaixo do peso.";
+            break;
+        case imc >= 18.5 && imc <= 24.9 :
+            resultado = "Peso normal";
+            break;
+        case imc >= 25 && imc <= 29.9 :
+            resultado = "Sobrepeso";
+            break;
+        case imc >= 30 && imc <= 34.9 :
+            resultado = "Obesidade Grau I (leve)";
+            break;
+        case imc >= 35 && imc <= 39.9 :
+            resultado = "Obesidade Grau II (moderada)";
+            break;
+        case imc >= 40:
+            resultado = "Obesidade Grau III (grave)";
+            break;
+        default:
+            resultado = "Operação inválida";
+    }
+
+    // Exibir o resultado na página
+    document.getElementById("resulte").innerHTML = "Resultado: " + resultado + " , seu IMC é " + imc.toFixed(2);
+}
+
+// Chamada da função calcular
+calcular();
